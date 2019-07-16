5ce7e659827d06b15a93e39cb47950d9f691c982
ahndwon <gahlok24@gmail.com>
2019-07-17 01:21:32 +0900

    ViewModel 적용

--- app/src/main/java/xyz/thingapps/rssliveslider/fragments/HomeFragment.kt
+++ app/src/main/java/xyz/thingapps/rssliveslider/fragments/HomeFragment.kt
@@ -6,23 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import xyz.thingapps.rssliveslider.R
 import xyz.thingapps.rssliveslider.adapters.FragmentListAdapter
-import xyz.thingapps.rssliveslider.models.RssItem
 import xyz.thingapps.rssliveslider.utils.Dummies
+import xyz.thingapps.rssliveslider.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         val adapter = FragmentListAdapter(childFragmentManager)
 
         adapter.fragments = listOf(
-                ChannelFragment.newInstance("Channel", Dummies.rssItems)
+            ChannelFragment.newInstance("Channel", Dummies.rssItems)
 //                ChannelFragment.newInstance("Channel", Dummies.rssItems),
 //                ChannelFragment.newInstance("Channel", Dummies.rssItems),
 //                ChannelFragment.newInstance("Channel", Dummies.rssItems),
@@ -37,4 +42,12 @@ class HomeFragment : Fragment() {
     }
 
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.let {
+            viewModel = ViewModelProviders.of(it).get(HomeViewModel::class.java)
+            viewModel.getRss()
+        }
+
+    }
 }
