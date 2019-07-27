290177a52d97c8bc7b54940561bf3a0a565fc0f6
ahndwon <gahlok24@gmail.com>
2019-07-28 00:06:46 +0900

    Refactor redundant code

--- app/src/main/java/xyz/thingapps/rssliveslider/api/dao/Cast.kt
+++ app/src/main/java/xyz/thingapps/rssliveslider/api/dao/Cast.kt
@@ -1,8 +1,6 @@
 package xyz.thingapps.rssliveslider.api.dao
 
 import com.tickaroo.tikxml.annotation.*
-import io.reactivex.subjects.PublishSubject
-
 
 
 @Xml(name = "rss")
@@ -11,11 +9,7 @@ data class Cast(
     @Path("channel") @PropertyElement(writeAsCData = true) val description: String? = "",
     @Path("channel") @PropertyElement(writeAsCData = true) val link: String? = "",
     @Path("channel") @Element val items: List<Item> = mutableListOf()
-) {
-    private val changeObservable = PublishSubject.create<Cast>()
-
-
-}
+)
 
 @Xml(name = "item")
 data class Item(
