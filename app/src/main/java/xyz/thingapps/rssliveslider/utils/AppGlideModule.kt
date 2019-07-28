8898988bd722e77ecde76c056e3b9f1d104f2c61
ahndwon <gahlok24@gmail.com>
2019-07-28 21:20:03 +0900

    Add AppGlideModule

---
+++ app/src/main/java/xyz/thingapps/rssliveslider/utils/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package xyz.thingapps.rssliveslider.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
