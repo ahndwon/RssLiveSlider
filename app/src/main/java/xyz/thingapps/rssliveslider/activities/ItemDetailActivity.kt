db810085d23968bdb953325949b31fdd934bae51
ahndwon <gahlok24@gmail.com>
2019-07-29 15:21:03 +0900

    Add button drawables

--- app/src/main/java/xyz/thingapps/rssliveslider/activities/ItemDetailActivity.kt
+++ app/src/main/java/xyz/thingapps/rssliveslider/activities/ItemDetailActivity.kt
@@ -1,13 +1,19 @@
 package xyz.thingapps.rssliveslider.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.activity_item_detail.*
 import xyz.thingapps.rssliveslider.R
+import xyz.thingapps.rssliveslider.activities.WebViewActivity.Companion.ITEM_URL
 import xyz.thingapps.rssliveslider.api.Item
+import java.util.concurrent.TimeUnit
 
 class ItemDetailActivity : AppCompatActivity() {
 
@@ -15,28 +21,38 @@ class ItemDetailActivity : AppCompatActivity() {
         const val RSS_ITEM = "rss_item"
     }
 
+    private val disposeBag = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_detail)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val item = intent.getParcelableExtra<Item>(RSS_ITEM)
+        val item: Item? = intent.getParcelableExtra<Item>(RSS_ITEM) ?: return
 
-        titleTextView.text = item.title
-        itemInfoTextView.text = item.pubDate
-        descriptionTextView.text = item.description
+        titleTextView.text = item?.title
+        itemInfoTextView.text = item?.pubDate
+        descriptionTextView.text = item?.description
 
-        Log.d(ItemDetailActivity::class.java.name, "media ${item.media}")
-
-        item.media?.let {
+        item?.media?.let {
             if (it.type.contains("image")) {
                 Glide.with(itemImageView)
                     .load(it.url)
                     .into(itemImageView)
-
             }
         }
+
+        visitButton.clicks().throttleFirst(600, TimeUnit.MILLISECONDS)
+            .subscribe({
+                item?.let {
+                    val intent = Intent(this, WebViewActivity::class.java)
+                    intent.putExtra(ITEM_URL, item.link)
+                    startActivity(intent)
+                }
+            }, { e ->
+                Log.d(ItemDetailActivity::class.java.name, "click button failed : ", e)
+            }).addTo(disposeBag)
 
     }
 
