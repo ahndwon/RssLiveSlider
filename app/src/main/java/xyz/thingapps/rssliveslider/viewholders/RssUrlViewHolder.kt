2bc53d4499e061698b368a426dbe24b4cf3b1155
ahndwon <gahlok24@gmail.com>
2019-07-30 20:43:56 +0900

    Add format date from epoch milli

--- app/src/main/java/xyz/thingapps/rssliveslider/viewholders/RssUrlViewHolder.kt
+++ app/src/main/java/xyz/thingapps/rssliveslider/viewholders/RssUrlViewHolder.kt
@@ -3,6 +3,11 @@ package xyz.thingapps.rssliveslider.viewholders
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_rss_url.view.*
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter
+import xyz.thingapps.rssliveslider.R
 import xyz.thingapps.rssliveslider.models.RssUrl
 
 class RssUrlViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
@@ -12,11 +17,20 @@ class RssUrlViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         with(view) {
             rssTitle.text = item.title
             rssUrl.text = item.url
-            rssCreatedAt.text = item.createdAt.toString()
+            rssCreatedAt.text = getAddedAt(item.createdAt)
             deleteButton.setOnClickListener {
                 onDeleteClick?.invoke(position)
             }
         }
+    }
+
+    private fun getAddedAt(createdAt: Long): String {
+        val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+        val date = ZonedDateTime.ofInstant(
+            Instant.ofEpochMilli(createdAt),
+            ZoneId.systemDefault()
+        ).format(formatter)
+        return view.context.getString(R.string.rss_add_at, date)
     }
 }
 
